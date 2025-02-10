42c9543b63a15f643be79a7e7b5918f86a77c7b0
Soheyl Mohammadi <soh3yl.md@gmail.com>
2025-02-10 16:19:18 +0330

    sending the lists of uploaded images and videos to templates in profile_view page

--- users/views.py
+++ users/views.py
@@ -7,6 +7,7 @@ from .forms import SignUpForm, CustomAuthenticationForm, UserUpdateForm
 # from django.db.models import Q
 from django.utils.translation import gettext as gt
 from django.utils.translation import gettext_lazy as gt_l
+from inserting.models import UploadImage, UploadVideo
 
 # from django.conf import settings
 # from django.utils import translation
@@ -22,8 +23,10 @@ from django.utils.translation import gettext_lazy as gt_l
 #     return_path = request.META.get('HTTP_REFERER', reverse('home'))
 #     return redirect(return_path)
 
+
 def home_view(request):
     return render(request, 'users/index.html', {"is_logged_in": request.user.is_authenticated})
+
 
 def signin_view(request):
     if request.method == 'POST':
@@ -36,15 +39,18 @@ def signin_view(request):
 
             if user is not None:
                 login(request, user)
-                messages.success(request, (gt("خوش آمدید %(name)s!") % {'name' : user.first_name}))
+                messages.success(
+                    request, (gt("خوش آمدید %(name)s!") % {'name': user.first_name}))
                 re = request.GET.get("next")
                 return redirect(re if re else 'home')
             else:
-                messages.error(request, gt_l("نام کاربری یا رمز عبور اشتباه است."))
+                messages.error(request, gt_l(
+                    "نام کاربری یا رمز عبور اشتباه است."))
     else:
         form = CustomAuthenticationForm()
 
     return render(request, 'users/login.html', {'form': form})
+
 
 def signup_view(request):
     if request.method == 'POST':
@@ -58,24 +64,34 @@ def signup_view(request):
         form = SignUpForm()
     return render(request, 'users/signup.html', {'form': form})
 
+
 @login_required
 def profile_view(request):
-    return render(request, 'users/profile.html', {'user': request.user})
+    user_images = request.user.uploaded_images.all().order_by('-upload_date')
+    user_videos = request.user.uploaded_videos.all().order_by('-upload_date')
+    return render(request, 'users/profile.html', {
+        'user': request.user,
+        'images': user_images,
+        'videos': user_videos
+    })
+
 
 @login_required
 def profile_edit_view(request):
     if request.method == 'POST':
-        form = UserUpdateForm(request.POST, request.FILES, instance=request.user)
+        form = UserUpdateForm(request.POST, request.FILES,
+                              instance=request.user)
         if form.is_valid():
             form.save()
-            messages.success(request, gt_l("پروفایل شما با موفقیت به‌روزرسانی شد."))
+            messages.success(request, gt_l(
+                "پروفایل شما با موفقیت به‌روزرسانی شد."))
             return redirect('profile')
     else:
         form = UserUpdateForm(instance=request.user)
     return render(request, 'user/profile_edit.html', {'form': form})
 
+
 def logout_view(request):
     logout(request)
     messages.info(request, gt_l("شما با موفقیت از حساب کاربری خارج شدید."))
     return redirect('home')
-
